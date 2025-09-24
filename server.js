74d6d64f0a652a5b887cfda5eda143cefe99cb20
Nurul Syahirah Abu Bakar <nurulsyahirahab@gmail.com>
2025-09-24 13:03:07 +0800

    Fix frontend serving

--- server.js
+++ server.js
@@ -1,67 +1,28 @@
 const express = require("express");
-const http = require("http");
-const socketIO = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
 
+const PORT = process.env.PORT || 3000;
+
+// Serve static frontend files from "public" folder
 app.use(express.static("public"));
 
-let players = {};
-let drawnNumbers = [];
+// Default route
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
-function generateCard() {
-  let numbers = [];
-  while (numbers.length < 25) {
-    let num = Math.floor(Math.random() * 75) + 1; // 1–75
-    if (!numbers.includes(num)) numbers.push(num);
-  }
-  return numbers;
-}
-
+// Socket.io connection
 io.on("connection", (socket) => {
-  console.log("Player connected:", socket.id);
-
-  // Assign bingo card
-  players[socket.id] = {
-    card: generateCard(),
-    marked: [],
-  };
-  socket.emit("card", players[socket.id].card);
-
-  // Draw number (only host for now)
-  socket.on("drawNumber", () => {
-    let num;
-    do {
-      num = Math.floor(Math.random() * 75) + 1;
-    } while (drawnNumbers.includes(num));
-
-    drawnNumbers.push(num);
-    io.emit("numberDrawn", num);
-  });
-
-  // Mark number
-  socket.on("markNumber", (num) => {
-    if (players[socket.id].card.includes(num)) {
-      players[socket.id].marked.push(num);
-      io.emit("marked", { player: socket.id, number: num });
-    }
-  });
-
-  // Check Bingo
-  socket.on("bingo", () => {
-    io.emit("winner", socket.id);
-    drawnNumbers = [];
-    players = {};
-  });
+  console.log("A player connected");
 
   socket.on("disconnect", () => {
-    console.log("Player disconnected:", socket.id);
-    delete players[socket.id];
+    console.log("A player disconnected");
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+// Start server
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
